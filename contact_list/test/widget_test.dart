79d107e8007da7a4278fcac765e24affb95d3e05
hou <41228208+cowFarmer@users.noreply.github.com>
2023-01-12 00:18:42 +0900

    2022.01.11 update homework_01, 02

--- contact_list/test/widget_test.dart
+++ contact_list/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:contact_list/main.dart';
+import 'package:contact_list/homework_01.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
