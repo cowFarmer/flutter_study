bff43404417a49b2d2f375ea78d2e6a812668037
hou <41228208+cowFarmer@users.noreply.github.com>
2023-03-05 14:22:36 +0900

    2023.03.05 update main

--- insta/lib/notification.dart
+++ insta/lib/notification.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 final notifications = FlutterLocalNotificationsPlugin();
 
@@ -52,4 +54,47 @@ showNotification() async {
       '내용1',
       NotificationDetails(android: androidDetails, iOS: iosDetails)
   );
+}
+
+showNotification2() async {
+
+  tz.initializeTimeZones();
+
+  var androidDetails = const AndroidNotificationDetails(
+    '유니크한 알림 ID',
+    '알림종류 설명',
+    priority: Priority.high,
+    importance: Importance.max,
+    color: Color.fromARGB(255, 255, 0, 0),
+  );
+  var iosDetails = const IOSNotificationDetails(
+    presentAlert: true,
+    presentBadge: true,
+    presentSound: true,
+  );
+
+  notifications.zonedSchedule(
+      2,
+      '제목2',
+      '내용2',
+      // makeDate(8, 30, 0), // 8시 30분 만드는 함수
+      tz.TZDateTime.now(tz.local).add(Duration(seconds: 5)), // 폰의 현재 시간을 기준으로 알림 주기 설정
+      NotificationDetails(android: androidDetails, iOS: iosDetails),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+    // matchDateTimeComponents: DateTimeComponents.time // 매일 같은 시간에 알림 띄우기 time 대신 dayOfWeekAndTime는 주 기준
+  );
+}
+
+
+
+makeDate(hour, min, sec){
+  var now = tz.TZDateTime.now(tz.local);
+  var when = tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, min, sec);
+  if (when.isBefore(now)) {
+    return when.add(Duration(days: 1));
+  } else {
+    return when;
+  }
 }
