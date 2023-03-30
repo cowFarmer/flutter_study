60beba57327298c999f83ea07cc5ff567201ac6a
hou <41228208+cowFarmer@users.noreply.github.com>
2023-03-30 13:00:06 +0900

    2023.03.30 json parsing

---
+++ air_pollution/lib/screen/const/data.dart
@@ -0,0 +1,2 @@
+// Decoding key
+const serviceKey = '1mKLdqL7F2/x00oX0Mulpw9CLKm528DOWPicPRtlYk21ZQn3NHpuPy9Ox4S/YgKUblbyD7WGVCwyBUAVRkLl9w==';
--- air_pollution/lib/screen/home_screen.dart
+++ air_pollution/lib/screen/home_screen.dart
@@ -4,11 +4,47 @@ import 'package:air_pollution/component/hourly_card.dart';
 import 'package:air_pollution/component/main_app_bar.dart';
 import 'package:air_pollution/component/main_card.dart';
 import 'package:air_pollution/component/main_drawer.dart';
+import 'package:air_pollution/model/stat_model.dart';
 import 'package:air_pollution/screen/const/colors.dart';
+import 'package:air_pollution/screen/const/data.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    final response = await Dio().get(
+      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+      queryParameters: {
+        'serviceKey': serviceKey,
+        'returnType': 'json',
+        'numOfRows': 30,
+        'pageNo': 1,
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+        'searchCondition': 'WEEK',
+      },
+    );
+
+    print(
+      response.data['response']['body']['items'].map(
+        (item) => StatModel.fromJson(json: item),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
