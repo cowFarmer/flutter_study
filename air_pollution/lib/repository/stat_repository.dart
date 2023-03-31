7fc46d6c388dd18dfded1a70c9f19fc8c011a4a0
hou <41228208+cowFarmer@users.noreply.github.com>
2023-03-31 12:52:24 +0900

    2023.03.31 status update

---
+++ air_pollution/lib/repository/stat_repository.dart
@@ -0,0 +1,24 @@
+import 'package:air_pollution/model/stat_model.dart';
+import 'package:air_pollution/screen/const/data.dart';
+import 'package:dio/dio.dart';
+
+class StatRepository {
+  static Future<List<StatModel>> fetchData() async {
+    final response = await Dio().get(
+      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+      queryParameters: {
+        'serviceKey': serviceKey,
+        'returnType': 'json',
+        'numOfRows': 30,
+        'pageNo': 1,
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+        'searchCondition': 'WEEK',
+      },
+    );
+
+    return response.data['response']['body']['items'].map<StatModel>(
+          (item) => StatModel.fromJson(json: item),
+    ).toList();
+  }
+}
